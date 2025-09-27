38d56cde72ad4aea762e51e7b8f163b8cb25e5c5
VaideshWaranR <vaiv64259@gmail.com>
2025-09-27 11:34:34 +0530

    kh

--- app/RealtimeLocationTracker.jsx
+++ app/RealtimeLocationTracker.jsx
@@ -33,7 +33,7 @@ export default function RealtimeLocationTracker() {
     Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) ** 2;
 
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  console.log(R*C+" distance in meters")
+  console.log(R*c+" distance in meters")
   return R * c; 
 };
 
